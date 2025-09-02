dbfbb1cdb7408e5e3bd51afadc1544f629b15b43
brian d foy <briandfoy@pobox.com>
2025-09-02 16:21:35 -0400

    initial creation

---
+++ lib/App/make_perl_symlinks.pm
@@ -0,0 +1,65 @@
+package App::make_perl_symlinks;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+App::make_perl_symlinks - Make version-specific sublinks to perl tools
+
+=head1 SYNOPSIS
+
+	use App::make_perl_symlinks;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/app-make_perl_symlinks
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2025, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
